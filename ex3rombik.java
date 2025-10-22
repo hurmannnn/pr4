96f553acb200b6c0391c660a05345808c5d1253a
hurmannnn <gurmanvika333@gmail.com>
2025-10-22 20:22:17 +0300

    Add files via upload

---
+++ ex3rombik.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ex3rombik {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть розмір ромба: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            scanner.close();
+        }
+    }
+}
